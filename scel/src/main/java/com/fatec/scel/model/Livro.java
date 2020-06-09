c165096db64e75a7ca3ea28e802e28cbdf68e2a2
RafaNot <RafaNot@DESKTOP-9EPI5KR>
2020-06-09 08:10:28 -0300

    Atualização da Classe Livro

--- scel/src/main/java/com/fatec/scel/model/Livro.java
+++ scel/src/main/java/com/fatec/scel/model/Livro.java
@@ -1,35 +1,38 @@
 package com.fatec.scel.model;
 
-import javax.persistence.*; //fornece aos desenvolvedores java facilidades para gerenciar objetos java e o banco de dados relacional
+import javax.persistence.*;
 import javax.validation.constraints.*;
 
-@Entity
+@Entity(name = "Livro")
 public class Livro {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
+	@NotNull
 	@Size(min = 4, max = 4, message = "ISBN deve ter 4 caracteres")
 	private String isbn;
-	
+	@NotNull
+	@Size(min = 1, max = 50, message = "Titulo deve ter entre 1 e 50 caracteres")
 	private String titulo;
-	
+	@NotNull
+	@Size(min = 1, max = 50, message = "Autor deve ter entre 1 e 50 caracteres")
 	private String autor;
 
 	public Livro() {
 	}
 
-	public Livro(String isbn, String titulo, String autor) {
-		this.isbn = isbn;
-		this.titulo = titulo;
-        this.autor = autor;
-	}
-
-	public Long getId() {
-		return id;
+	public Livro(String i, String t, String a) {
+		this.isbn = i;
+		this.titulo = t;
+		this.autor = a;
 	}
 
 	public void setId(Long id) {
 		this.id = id;
+	}
+
+	public Long getId() {
+		return id;
 	}
 
 	public String getIsbn() {
@@ -55,6 +58,4 @@ public class Livro {
 	public void setAutor(String autor) {
 		this.autor = autor;
 	}
-	
-	
 }
