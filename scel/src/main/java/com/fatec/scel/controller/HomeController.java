27f7e8a28e12987d7a38333ff8f15371821adee3
RafaNot <RafaNot@DESKTOP-9EPI5KR>
2020-07-07 04:09:31 -0300

    Sistema de Login Implementado

--- scel/src/main/java/com/fatec/scel/controller/HomeController.java
+++ scel/src/main/java/com/fatec/scel/controller/HomeController.java
@@ -6,6 +6,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 	
+	@GetMapping("/login")
+	public String login() {
+	 return "login";
+	}
+	
 	@GetMapping("/home")
     public String home() {
         return "home";
---
+++ scel/src/main/java/com/fatec/scel/security/WebSecurityConfig.java
@@ -0,0 +1,23 @@
+package com.fatec.scel.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+		.anyRequest()
+		.authenticated()
+		.and()
+		.formLogin()
+		.loginPage("/login")
+		.permitAll();
+	}
+}
